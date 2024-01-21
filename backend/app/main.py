4c78825ec2b236c775097237c3f86170d49a9067
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2024-01-21 14:42:50 +0100

    Seeding API

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,16 +4,78 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from . import models, schemas, admin
 from typing import Tuple, List 
+from datetime import datetime
 import logging
 import sys
 import os
+from faker import Faker
+import random
 
 from .database import SessionLocal, engine, get_db, Database
 from sqlalchemy.orm import Session
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+# Recreate the database tables, load fixtures on restart
+models.Base.metadata.drop_all(bind=engine)
 models.Base.metadata.create_all(bind=engine)
+
+bowling_alleys_data = [
+    {"id": 1, "name": "Wronki"},
+    {"id": 2, "name": "Leszno"},
+    {"id": 3, "name": "Sieraków"},
+    {"id": 4, "name": "Tarnowo Podgórne"},
+    {"id": 5, "name": "Poznań"},
+    {"id": 6, "name": "Łaziska Górne"},
+    {"id": 7, "name": "Puck"},
+    {"id": 8, "name": "Gostyń"},
+]
+tournaments_data = [
+    {"id": 1, "name": "Mistrzostwa Polski Juniorów 2024", "bowling_alley_id": 5, "finished": False,
+     "date_start": datetime(2024, 1, 20), "date_end": datetime(2024, 1, 28)},
+    {"id": 2, "name": "4. Memoriał Ryszarda Bonka", "bowling_alley_id": 4, "finished": True,
+     "date_start": datetime(2023, 1, 1), "date_end": datetime(2023, 1, 3)},
+    {"id": 3, "name": "8. Puchar Starosty w Sierakowie", "bowling_alley_id": 3, "finished": True,
+     "date_start": datetime(2023, 6, 11), "date_end": datetime(2023, 6, 15)},
+]
+
+fake = Faker(locale = 'pl_PL')
+
+# Seed TournamentScore table
+tournament_scores_data = []
+
+for tournament_id in range(1, 4):  # Assuming you have tournaments with IDs 1, 2, and 3
+    for _ in range(random.randint(5, 15)):
+        #tournament_start_date = tournaments_data[tournament_id - 1]['date_start']
+        #tournament_end_date = tournaments_data[tournament_id - 1]['date_end']
+        score_data = {
+            "score": random.randint(400, 650),
+            #"notes": fake.text(),
+            #"private_notes": fake.text(),
+            "tournament_id": tournament_id,
+            "name": fake.first_name(),
+            "surname": fake.last_name(),
+        }
+        tournament_scores_data.append(score_data)
+
+with SessionLocal() as db:
+    for alley_data in bowling_alleys_data:
+        bowling_alley = models.BowlingAlley(**alley_data)
+        db.add(bowling_alley)
+    db.commit()
+    
+    for tournament_data in tournaments_data:
+        tournament = models.Tournament(**tournament_data)
+        db.add(tournament)
+    db.commit()
+
+    for score_data in tournament_scores_data:
+        tournament_score = models.TournamentScore(**score_data)
+        db.add(tournament_score)
+    db.commit()
+
+
 
 # Source: https://stackoverflow.com/questions/63069190/how-to-capture-arbitrary-paths-at-one-route-in-fastapi
 # Created by Noah Cardoza
@@ -69,7 +131,6 @@ static_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "sta
 
 @app.get("/api/v1/bowling_alleys/", response_model=None)
 def get_all_bowling_alleys(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)) -> list[models.BowlingAlley]:
-    print("Dupa!")
     bowling_alleys = db.query(models.BowlingAlley).offset(skip).limit(limit).all()
     return bowling_alleys
 
@@ -102,7 +163,7 @@ def get_tournament_scores(tournament_id: int, db: Session = Depends(get_db)) -> 
     if tournament is None:
         raise HTTPException(status_code=404, detail="Tournament not found")
 
-    scores = db.query(models.TournamentScore).filter(models.TournamentScore.tournament_id == tournament.id).all()
+    scores = db.query(models.TournamentScore).filter(models.TournamentScore.tournament_id == tournament.id).order_by(models.TournamentScore.score.desc()).all()
     return scores
 
 
